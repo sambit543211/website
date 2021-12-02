511e3bcd8f38345706f931e8ec310e38dd2006cc
sambit543211 <cs.sambit@gmail.com>
2021-12-02 23:52:51 +0530

    new edit

---
+++ src/pages/writing2.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react' ;
+import Navbar from '../components/NavbarB';
+import Sidebar from '../components/SidebarB';
+import Content from '../components/Page2';
+import ScrollToTop from '../components/ScrollToTop';
+import Footer from '../components/Footer';
+
+
+
+const Writing2 = () => {
+
+    const [isOpen, setIsOpen] = useState(false)
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+    return (
+        <>
+            <ScrollToTop/>
+            <Sidebar isOpen={isOpen} toggle={toggle}/>
+            <Navbar toggle={toggle}/>
+            <Content/>
+            <Footer />
+        </>
+    );
+};
+
+export default Writing2;
