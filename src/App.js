71c3e18bea126c86190113cc78a8f3851cd7c62b
sambit543211 <cs.sambit@gmail.com>
2021-12-02 23:54:31 +0530

    yo

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import React from 'react';
 import  {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './pages'
-
 import Writing from './pages/writing';
+import Writing2 from './pages/writing2';
+import Writing3 from './pages/writing3';
 import SuchiPage from './pages/suchi';
 import TeamPage from './pages/team';
 
@@ -17,6 +18,8 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/signin" component={SuchiPage}/>
         <Route exact path="/writing" component={Writing}/>
+        <Route exact path="/writing2" component={Writing2}/>
+        <Route exact path="/writing3" component={Writing3}/>
         <Route exact path="/team" component={TeamPage}/>
 
       </Switch>
--- src/components/Suchi/index.js
+++ src/components/Suchi/index.js
@@ -10,10 +10,10 @@ const Suchi = () => {
                 <SuchiList to="/writing">1. ଅନେକ ସ୍ମିତ ହସ -- ମନୋଜ ଦାସ  </SuchiList>
             </SuchiP>
             <SuchiP>
-                <SuchiList>Link2</SuchiList>
+                <SuchiList to="/writing2">2. ଯିଏ ଅଗ୍ନିରୁ ଜନ୍ମ ହୁଏ -- ପ୍ରତୀ ରାୟ </SuchiList>
             </SuchiP>
             <SuchiP>
-                <SuchiList>Link3</SuchiList>
+                <SuchiList to="/writing3">3. ରେବତୀ -- ଫକିର ମୋହନ ସେନାପତି</SuchiList>
             </SuchiP>
             
         </SuchiContainer>
